aba8ee9f7ecb6b5265aa8c388c3c48d1cc6c85ed
Miguel Santos <130518190+MiguelMfs@users.noreply.github.com>
2025-02-07 21:41:40 -0300

    Update dashboard.php

--- Questão 1/dashboard.php
+++ Questão 1/dashboard.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user_id'])) { // Verifica se o usuário está logado
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Área do Morador</title>
+    <title>Area do Morador</title>
 </head>
 <body>
     <h2>Bem-vindo, <?php echo $_SESSION['nome']; ?>!</h2>
